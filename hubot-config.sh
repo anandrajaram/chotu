888236eafd0926c6db12e3471ad32faac8f84491
Anand Rajaram <arajaram@hubspot.com>
2014-11-28 10:32:43 -0500

    ahem, security fixes

--- hubot-config.sh
+++ hubot-config.sh
@@ -1,10 +1,10 @@
 # Slack specifics
 heroku config:add HEROKU_URL=https://mades-slackbot.herokuapp.com/
-heroku config:add HUBOT_SLACK_TOKEN=Su4aCeRcbjpIUwnqoKJGLucw
+heroku config:add HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN
 heroku config:add HUBOT_SLACK_TEAM=mades
 heroku config:add HUBOT_SLACK_BOTNAME=chotu
 
 # Darksky script
 
-heroku config:add HUBOT_DARK_SKY_API_KEY=6ad495e01fa226e9dc1b111bc19bb536
+heroku config:add HUBOT_DARK_SKY_API_KEY=$HUBOT_DARK_SKY_API_KEY
 heroku config:add HUBOT_DARK_SKY_DEFAULT_LOCATION=Boston
